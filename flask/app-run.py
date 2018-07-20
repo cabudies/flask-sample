d77a051a78652c45231e0946a42b6fd6d948088d
Gurjas Singh <gurjassingh92@gmail.com>
2018-07-20 11:14:03 +0530

    1. Added navbar to 'PageNotFound.html' 2. Added bootstrap to our website. 3. Adding Modal button that appears on the click on Login anchor tag. 4. Understanding the bootstrap css and designing our website.

--- flask/app-run.py
+++ flask/app-run.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-# import flask
 
 app = Flask(__name__)
 
@@ -8,9 +7,13 @@ def index():
     # return "Welcome to python."
     return render_template('index.html')
 
-@app.route('/hello')
-def helloWorld():
-    return "Hello World. This is an return value."
+@app.route('/hello/<string:username>/')
+def helloUsername(username):
+    return render_template('hello.html', name = username)
+
+# @app.route('/hello')
+# def helloWorld():
+#     return "Hello World. This is an return value."
 
 @app.errorhandler(404)
 def pageNotFound(error):
